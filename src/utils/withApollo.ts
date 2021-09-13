58d1bcac351357fa118ed9a798eb0ecf3b724994
Your Name <you@example.com>
2021-09-13 17:27:44 -0400

    first commit

--- src/utils/withApollo.ts
+++ src/utils/withApollo.ts
@@ -2,7 +2,7 @@ import { withApollo } from 'next-apollo';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: 'https://trivia-contest.herokuapp.com/',
+  uri: 'https://trivia-contest.herokuapp.com/graphql',
   cache: new InMemoryCache({})
 });
 
